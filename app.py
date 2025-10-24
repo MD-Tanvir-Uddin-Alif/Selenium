649da9b8b626257a1ecbd3df8f4d915c8fd503cc
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-10-25 00:01:03 +0600

    going through selenium

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello")
+from selenium import webdriver
+from selenium.webdriver.common.keys import  Keys
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+driver.get("http://www.python.org")
+
+# print(driver.title)
+
+
+# assert "Python" in driver.title
+self.assertIn("Python", driver.title)
